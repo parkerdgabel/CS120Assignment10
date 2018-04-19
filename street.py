f1557bdc4bcd175710a31b45a3d17217770cb268
parkerdgabel <parker.d.gabel@gmail.com>
2018-04-18 17:32:39 -0700

    Added some stuff

--- street.py
+++ street.py
@@ -7,6 +7,7 @@ class StreetStructure:
     def at_height(self, height=0):
         pass
 
+
 class Building(StreetStructure):
     def __init__(self, width=0, height=0, brick=""):
         super().__init__(width, height, brick)
@@ -28,15 +29,20 @@ class Park(StreetStructure):
         if 5 < height:
             return " " * self._char
         elif height in range(2):
-            return (" " * (self._width // 2)) + "|" + (" " * (self._width // 2))
+            return (" " * (self._width // 2)) + "|" + (" " *
+                                                       (self._width // 2))
         else:
             if height == 4:
-                return (" " * (self._width // 2)) + self._char + (" " * (self._width // 2))
+                return (" " *
+                        (self._width // 2)) + self._char + (" " *
+                                                            (self._width // 2))
 
             elif height == 3:
-                return (" " * (self._width // 2 - 1)) + self._char * 3 + (" " * (self._width // 2 - 1))
+                return (" " * (self._width // 2 - 1)) + self._char * 3 + (
+                    " " * (self._width // 2 - 1))
             else:
-                return (" " * (self._width // 2 - 2)) + self._char * 5 + (" " * (self._width // 2 - 2))
+                return (" " * (self._width // 2 - 2)) + self._char * 5 + (
+                    " " * (self._width // 2 - 2))
 
 
 class EmptyLot(StreetStructure):
@@ -47,12 +53,22 @@ class EmptyLot(StreetStructure):
         if 0 < height:
             return " " * self._width
         else:
-            return (self._char * self._width)[:self._width + 1].replace("_", " ")
+            return (self._char * self._width)[:self._width + 1].replace(
+                "_", " ")
+
 
 class Street:
     def __init__(self, street_string):
         self._street = build_street_list(street_string.split())
-        self._height = max_height(self._street)
+        self._height = max_height(self._street) + 1
+
+
+def street_at_height(street_list, height, ret_str=""):
+    if street_list == []:
+        return ret_str
+    else:
+        ret_str += street_list[0].at_height(height)
+        return street_at_height(street_list[1:], height, ret_str)
 
 
 def build_street_list(street_list, lst=[]):
@@ -70,11 +86,10 @@ def build_street_list(street_list, lst=[]):
             lst.append(EmptyLot(int(info[0]), info[1]))
         return build_street_list(street_list, lst)
 
+
 def max_height(lst=[], max_num=0):
     if lst == []:
         return max_num
     else:
         max_num = max(lst[0]._height, max_num)
         return total_height(lst[1:], max_num)
-
-
